6349d328e69ddfed6c93057d9c6b9c6bb5aa3223
Adrien Tiburce <adrien.tiburce@gmail.com>
2019-08-12 15:34:06 +0200

    (Fix) course fixtures remove duration & add slug

--- src/DataFixtures/CourseFixtures.php
+++ src/DataFixtures/CourseFixtures.php
@@ -15,39 +15,39 @@ class CourseFixtures extends Fixture
 
         $courseCate1 = new CourseCategory();
         $courseCate1->setName('Tronc Commun')
-                    ->setSemester("S5")
-                    ->setPromotion("G1");
+            ->setSlug('tronc-commun')
+            ->setSemester("S5")
+            ->setPromotion("G1");
         for ($i = 0; $i < mt_rand(3, 5); $i++) {
             $course = new Course();
             $course->setName($faker->company)
-                    ->setDuration($faker->dateTime)
-                    ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
+                ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
             $courseCate1->addCourse($course);
             $manager->persist($course);
         }
 
         $courseCate2 = new CourseCategory();
         $courseCate2->setName("Electifs d'Integration")
-                    ->setSemester("S7")
-                    ->setPromotion("G2");
+            ->setSlug('electifs-integration')
+            ->setSemester("S7")
+            ->setPromotion("G2");
         for ($i = 0; $i < mt_rand(3, 5); $i++) {
             $course = new Course();
             $course->setName($faker->company)
-                    ->setDuration($faker->dateTime)
-                    ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
+                ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
             $courseCate2->addCourse($course);
             $manager->persist($course);
         }
 
         $courseCate3 = new CourseCategory();
         $courseCate3->setName("Electifs Disciplinaires")
-                     ->setSemester("S6/S8")
-                     ->setPromotion("G1/G2");
+            ->setSlug('electif-disciplinaires')
+            ->setSemester("S6/S8")
+            ->setPromotion("G1/G2");
         for ($i = 0; $i < mt_rand(3, 5); $i++) {
             $course = new Course();
             $course->setName($faker->company)
-                    ->setDuration($faker->dateTime)
-                    ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
+                ->setDescription($faker->realText($maxNbChars = 80, $indexSize = 2));
             $courseCate3->addCourse($course);
             $manager->persist($course);
         }
@@ -58,8 +58,8 @@ class CourseFixtures extends Fixture
     }
 }
 // SOR	Sociologie des organisations-
-// TSI	Traitement du signal	
-// CMO	Complexité - Modélisation	
+// TSI	Traitement du signal
+// CMO	Complexité - Modélisation
 // AAP	Algorithmique avancée et programmation
 // AIN	Analyse pour l'ingénieur
 // MMC	Mécanique de milieux continus
